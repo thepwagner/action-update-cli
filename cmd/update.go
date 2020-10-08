5e65594f3b56d76d698637ae405cc07c9ff58865
Peter Wagner <git@mycloudand.me>
2020-10-07 21:34:26 -0400

    load config from actions

--- cmd/update.go
+++ cmd/update.go
@@ -11,6 +11,8 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/caarlos0/env/v6"
+	"github.com/ghodss/yaml"
 	"github.com/google/go-github/v32/github"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -18,7 +20,6 @@ import (
 	"github.com/thepwagner/action-update-docker/docker"
 	"github.com/thepwagner/action-update-dockerurl/dockerurl"
 	"github.com/thepwagner/action-update-go/gomodules"
-	"github.com/thepwagner/action-update/actions"
 	"github.com/thepwagner/action-update/actions/updateaction"
 	"github.com/thepwagner/action-update/cmd"
 	gitrepo "github.com/thepwagner/action-update/repo"
@@ -68,49 +69,57 @@ func MockUpdate(ctx context.Context, target string) error {
 		}()
 	}
 
-	cfg, err := cloneAndConfig(ctx, target, dir)
-	if err != nil {
-		return err
-	} else if cfg == nil {
-		return fmt.Errorf("could not detect environment")
-	}
-	dirLog.Info("cloned to tempdir")
-	cfg.NoPush = true
-
-	if err := os.Chdir(dir); err != nil {
-		return err
-	}
-
-	var env updateaction.HandlerParams
-	switch updaterType {
-	case "docker":
-		env = &docker.Environment{}
-	case "dockerurl":
-		env = &dockerurl.Environment{}
-	case "go":
-		env = &gomodules.Environment{}
-	default:
-		return fmt.Errorf("unknown updater: %w", err)
-	}
-
-	handlers := updateaction.NewHandlers(env)
-	return handlers.Handle(ctx, &cfg.Environment)
-}
-
-func cloneAndConfig(ctx context.Context, target, dir string) (*updateaction.Environment, error) {
 	parsed, err := parseTargetURL(target)
 	if err != nil {
-		return nil, err
-	}
-
-	if err := parsed.initRepo(ctx, dir); err != nil {
-		return nil, err
+		return err
 	}
 
 	ghToken := viper.GetString(cfgGitHubToken)
 	gh := gitrepo.NewGitHubClient(ghToken)
 
-	// TODO: find self-references in .github/workflows/*.yaml to guess configuration?
+	ok, err := cloneAndSetEnv(ctx, parsed, gh, dir)
+	if err != nil {
+		return err
+	} else if !ok {
+		return fmt.Errorf("could not detect environment")
+	}
+	dirLog.Info("cloned to tempdir")
+
+	if err := os.Chdir(dir); err != nil {
+		return err
+	}
+
+	var environment *updateaction.Environment
+	var params updateaction.HandlerParams
+	switch updaterType {
+	case "docker":
+		e := &docker.Environment{}
+		environment = &e.Environment
+		params = e
+	case "dockerurl":
+		e := &dockerurl.Environment{}
+		environment = &e.Environment
+		params = e
+	case "go":
+		e := &gomodules.Environment{}
+		environment = &e.Environment
+		params = e
+	default:
+		return fmt.Errorf("unknown updater: %w", err)
+	}
+
+	if err := loadConfiguration(ctx, gh, parsed, params); err != nil {
+		return err
+	}
+
+	handlers := updateaction.NewHandlers(params)
+	return handlers.Handle(ctx, &environment.Environment)
+}
+
+func cloneAndSetEnv(ctx context.Context, parsed *parsedTarget, gh *github.Client, dir string) (bool, error) {
+	if err := parsed.initRepo(ctx, dir); err != nil {
+		return false, err
+	}
 
 	// Interpret the path to decide how to clone and what event to simulate:
 	if parsed.prNumber > 0 {
@@ -120,6 +129,80 @@ func cloneAndConfig(ctx context.Context, target, dir string) (*updateaction.Envi
 
 	// Fetch default branch and simulate `schedule` to reopen all
 	return parsed.cloneEvent(ctx, dir)
+}
+
+const workflowDir = ".github/workflows"
+
+func loadConfiguration(ctx context.Context, gh *github.Client, parsed *parsedTarget, environment interface{}) error {
+	_, directoryContent, _, err := gh.Repositories.GetContents(ctx, parsed.owner, parsed.repo, workflowDir, &github.RepositoryContentGetOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, f := range directoryContent {
+		file, _, _, err := gh.Repositories.GetContents(ctx, parsed.owner, parsed.repo, path.Join(workflowDir, f.GetName()), &github.RepositoryContentGetOptions{})
+		if err != nil {
+			return err
+		}
+		content, err := file.GetContent()
+		if err != nil {
+			return err
+		}
+
+		data := map[string]interface{}{}
+		if err := yaml.Unmarshal([]byte(content), &data); err != nil {
+			return err
+		}
+
+		jobs, ok := data["jobs"].(map[string]interface{})
+		if !ok {
+			continue
+		}
+		for jobName, jobDetails := range jobs {
+			jd := jobDetails.(map[string]interface{})
+			steps, ok := jd["steps"].([]interface{})
+			if !ok {
+				continue
+			}
+
+			for i, stepDetails := range steps {
+				step := stepDetails.(map[string]interface{})
+				uses, ok := step["uses"].(string)
+				if !ok {
+					continue
+				}
+				if !strings.HasPrefix(uses, "thepwagner/action-update") {
+					continue
+				}
+				with, ok := step["with"].(map[string]interface{})
+				if !ok {
+					continue
+				}
+				logrus.WithFields(logrus.Fields{
+					"filename": f.GetName(),
+					"job":      jobName,
+					"step":     i,
+				}).Info("loaded configuration from workflow")
+
+				for k, v := range with {
+					key := fmt.Sprintf("INPUT_%s", strings.ToUpper(k))
+					value := fmt.Sprintf("%v", v)
+					if strings.Contains(value, "${{") {
+						continue
+					}
+					logrus.WithFields(logrus.Fields{
+						"key":   key,
+						"value": value,
+					}).Debug("setting value from workflow input")
+					_ = os.Setenv(key, value)
+				}
+				return env.Parse(environment)
+			}
+		}
+	}
+
+	logrus.Info("could not load configuration from repo")
+	return nil
 }
 
 type parsedTarget struct {
@@ -164,21 +247,21 @@ func (p *parsedTarget) initRepo(ctx context.Context, dir string) error {
 	return nil
 }
 
-func (p *parsedTarget) clonePullRequest(ctx context.Context, gh *github.Client, dir string) (*updateaction.Environment, error) {
+func (p *parsedTarget) clonePullRequest(ctx context.Context, gh *github.Client, dir string) (bool, error) {
 	// pull request - fetch the pr HEAD and simulate a "reopened" event
 	pr, _, err := gh.PullRequests.Get(ctx, p.owner, p.repo, p.prNumber)
 	if err != nil {
-		return nil, fmt.Errorf("getting PR: %w", err)
+		return false, fmt.Errorf("getting PR: %w", err)
 	}
 
 	remoteRef := fmt.Sprintf("refs/remotes/pull/%d/merge", p.prNumber)
 	refSpec := fmt.Sprintf("+%s:%s", pr.GetMergeCommitSHA(), remoteRef)
 	if err := cmd.CommandExecute(ctx, dir, "git", "-c", "protocol.version=2", "fetch", "--no-tags",
 		"--prune", "--progress", "--no-recurse-submodules", "--depth=1", gitrepo.RemoteName, refSpec); err != nil {
-		return nil, fmt.Errorf("git fetch: %w", err)
+		return false, fmt.Errorf("git fetch: %w", err)
 	}
 	if err := cmd.CommandExecute(ctx, dir, "git", "checkout", "--progress", "--force", remoteRef); err != nil {
-		return nil, fmt.Errorf("git fetch: %w", err)
+		return false, fmt.Errorf("git fetch: %w", err)
 	}
 
 	tmpEvt, err := tmpEventFile(&github.PullRequestEvent{
@@ -186,33 +269,27 @@ func (p *parsedTarget) clonePullRequest(ctx context.Context, gh *github.Client, 
 		PullRequest: pr,
 	})
 	if err != nil {
-		return nil, fmt.Errorf("creating temp event file: %w", err)
+		return false, fmt.Errorf("creating temp event file: %w", err)
 	}
-	return &updateaction.Environment{
-		Environment: actions.Environment{
-			GitHubEventName: "pull_request",
-			GitHubEventPath: tmpEvt,
-		},
-	}, nil
+	_ = os.Setenv("GITHUB_EVENT_NAME", "pull_request")
+	_ = os.Setenv("GITHUB_EVENT_PATH", tmpEvt)
+	return true, nil
 }
 
-func (p *parsedTarget) cloneEvent(ctx context.Context, dir string) (*updateaction.Environment, error) {
+func (p *parsedTarget) cloneEvent(ctx context.Context, dir string) (bool, error) {
 	branchName := viper.GetString(cfgBranchName)
 	remoteRef := path.Join("refs/remotes/origin", branchName)
 	refSpec := fmt.Sprintf("+:%s", remoteRef)
 	if err := cmd.CommandExecute(ctx, dir, "git", "-c", "protocol.version=2", "fetch", "--no-tags",
 		"--prune", "--progress", "--no-recurse-submodules", "--depth=1", gitrepo.RemoteName, refSpec); err != nil {
-		return nil, fmt.Errorf("git fetch: %w", err)
+		return false, fmt.Errorf("git fetch: %w", err)
 	}
 	if err := cmd.CommandExecute(ctx, dir, "git", "checkout", "--progress", "--force", "-B", branchName, remoteRef); err != nil {
-		return nil, fmt.Errorf("git fetch: %w", err)
+		return false, fmt.Errorf("git fetch: %w", err)
 	}
 
-	return &updateaction.Environment{
-		Environment: actions.Environment{
-			GitHubEventName: "schedule",
-		},
-	}, nil
+	_ = os.Setenv("GITHUB_EVENT_NAME", "schedule")
+	return true, nil
 }
 
 func tmpEventFile(evt interface{}) (string, error) {
